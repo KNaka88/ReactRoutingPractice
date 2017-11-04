10d2489ead37dd9592d7f23137db06bd53c1a3ca
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-11-04 12:52:00 -0700

    create posts_index.js and cleanup code

--- src/index.js
+++ src/index.js
@@ -4,19 +4,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-
-class Hello extends React.Component {
-  render() { return <div>Hello</div> }
-}
-
-class Goodbye extends React.Component {
-  render() { return <div>Goodbye</div> }
-}
 
 
 
@@ -25,9 +15,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        Header
-        <Route path="/hello" component={Hello} />
-        <Route path="/goodbye"  component={Goodbye} />
+
       </div>
     </BrowserRouter>
   </Provider>
