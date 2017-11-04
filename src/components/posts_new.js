feb6928938f4a4982e4ecb14bcb8d1780de7e551
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-11-04 14:53:45 -0700

    add input form in PostsNew component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  render() {
+  renderTitleField(field) { //contains event handler wireup jsx
     return (
       <div>
-        Posts New!
+        <input
+          type="text"
+          {...field.input}
+          />
       </div>
+    );
+  }
+
+
+  render() {
+    return (
+      <form>
+        <Field
+          name="title"
+          component={this.renderTitleField}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm' // you can name multiple form names (ex. signup and signin form)
+})(PostsNew);
